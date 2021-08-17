9e5e3e7838e4249055af4f1dd1ce192da7607dac
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-08-17 16:04:03 -0300

    implementado barra de busca
    eh possivel fazer buscas na aba comunidade

--- js/controller/ComunidadeController.js
+++ js/controller/ComunidadeController.js
@@ -7,6 +7,8 @@ class ComunidadeController{
 
         this._menu = document.querySelector('.menu-mobile-aba');
 
+
+        this._posts;
     }
 
     listaPosts(){
@@ -19,12 +21,35 @@ class ComunidadeController{
             ).then(model => 
                 this._comunidadeView.update(model, this._codigosWrapper)
             ).then(() => this.aplicarHighlight())
-                .catch(erro => console.log(erro))
+            .then(() => this._posts = document.querySelectorAll('.post'))
+            .catch(erro => console.log(erro))
+    }
+
+    buscarPost(event){
+        let pesquisa = event;
+        
+        if(pesquisa.length > 0){
+            this._posts.forEach( post => {
+                var titulo = post.querySelector('h2').textContent;
+                
+                let expressao = new RegExp(pesquisa, "i");
+                
+                if(!expressao.test(titulo)){
+                    post.classList.add("invisivel")
+                } else {
+                    post.classList.remove("invisivel")
+                }
+            })
+        } else {
+            this._posts.forEach( post => {
+                post.classList.remove("invisivel");
+            })
+        }
+        
     }
 
     aplicarHighlight(){
         let codigoCache =  this._localDosPosts.querySelectorAll('code');
-        console.log(codigoCache);
         codigoCache.forEach(tag => {
             hljs.highlightElement(tag);
         })
